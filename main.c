b4592868fad5290495f52d679bea179f212e56eb
ezeppa <ezeppa@Debian.myguest.virtualbox.org>
2025-01-30 18:34:14 +0100

    Norminette OK

--- main.c
+++ main.c
@@ -75,7 +75,36 @@ typedef struct s_app
 	t_entity	*exit;
 	int			nb_collectibles;
 	t_entity	**collectibles;
+	int			nb_moves;
 }	t_app;
+
+int	exit_program(t_app *app)
+{
+	mlx_destroy_image(app->mlx, app->imgs.empty.img);
+	mlx_destroy_image(app->mlx, app->imgs.player_a1.img);
+	mlx_destroy_image(app->mlx, app->imgs.player_a2.img);
+	mlx_destroy_image(app->mlx, app->imgs.wall.img);
+	mlx_destroy_image(app->mlx, app->imgs.collectible.img);
+	mlx_destroy_image(app->mlx, app->imgs.exit.img);
+	if (app->mlx_win)
+		mlx_destroy_window(app->mlx, app->mlx_win);
+	if (app->mlx)
+	{
+		mlx_destroy_display(app->mlx);
+		free(app->mlx);
+	}
+	for (int i = 0; i < app->map.height; i++)
+		free(app->map.cells[i]);
+	free(app->map.cells);
+	free(app->player);
+	free(app->exit);
+	for (int i = 0; i < 100; i++)
+		if (app->collectibles[i])
+			free(app->collectibles[i]);
+	free(app->collectibles);
+	exit(0);
+	return (0);
+}
 
 void	init_imgs(t_app *app)
 {
@@ -123,13 +152,43 @@ void	add_exit(t_app *app, int x, int y)
 	app->exit->y = y;
 }
 
+char	*get_file_str(char *filename)
+{
+	int		fd;
+	char	*file_str;
+	char	*tmp_str;
+	char	buf[2];
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	file_str = malloc(1);
+	file_str[0] = '\0';
+	while (read(fd, buf, 1) > 0)
+	{
+		buf[1] = '\0';
+		tmp_str = file_str;
+		file_str = ft_strjoin(file_str, buf);
+		free(tmp_str);
+	}
+	close(fd);
+	return (file_str);
+}
+
 void	init_map(t_app *app, char *filename)
 {
-	char *map_str = "1111111111111\n10010000000C1\n1000011111001\n1P0011E000001\n1111111111111";
+	char *map_str = get_file_str(filename);
+	if (!map_str)
+	{
+		ft_printf("Error\nUnable to open the file.\n");
+		exit_program(app);
+	}
 	char **split = ft_split(map_str, '\n');
 
 	app->map.width = ft_strlen(split[0]);
-	app->map.height = 5;
+	app->map.height = 0;
+	for (int i = 0; split[i]; i++)
+		app->map.height++;
 	app->map.cells = malloc(app->map.height * sizeof(t_cell *));
 	for (int i = 0; i < app->map.height; i++)
 	{
@@ -155,6 +214,13 @@ void	init_map(t_app *app, char *filename)
 		free(split[i]);
 	}
 	free(split);
+	free(map_str);
+}
+
+void	refresh_screen(t_app *app)
+{
+	mlx_destroy_window(app->mlx, app->mlx_win);
+	app->mlx_win = mlx_new_window(app->mlx, app->map.width * 32, app->map.height * 32, "so_long");
 }
 
 void	init_app(t_app *app)
@@ -162,16 +228,13 @@ void	init_app(t_app *app)
 	app->mlx = mlx_init();
 	app->mlx_win = mlx_new_window(app->mlx, 800, 800, "so_long");
 	
+	app->nb_moves = 0;
 	app->nb_collectibles = 0;
 	app->collectibles = malloc(sizeof(t_entity *) * 100);
 	for (int i = 0; i < 100; i++)
 		app->collectibles[i] = NULL;
 	app->player = malloc(sizeof(t_entity));
 	app->exit = malloc(sizeof(t_entity));
-	
-	init_map(app, "base.ber");
-	mlx_destroy_window(app->mlx, app->mlx_win);
-	app->mlx_win = mlx_new_window(app->mlx, app->map.width * 32, app->map.height * 32, "so_long");
 	init_imgs(app);
 }
 
@@ -194,40 +257,19 @@ void	draw_map(t_app *app)
 
 void	draw_entities(t_app *app)
 {
-	mlx_put_image_to_window(app->mlx, app->mlx_win, app->exit->a1->img, app->exit->x * 32, app->exit->y * 32);
-	for (int i = 0; app->collectibles[i]; i++)
-	{
-		mlx_put_image_to_window(app->mlx, app->mlx_win, app->collectibles[i]->a1->img, app->collectibles[i]->x * 32, app->collectibles[i]->y * 32);
-	}
-	mlx_put_image_to_window(app->mlx, app->mlx_win, app->player->a1->img, app->player->x * 32, app->player->y * 32);
-}
+	static int time_anim = 0;
 
-int	exit_program(t_app *app)
-{
-	mlx_destroy_image(app->mlx, app->imgs.empty.img);
-	mlx_destroy_image(app->mlx, app->imgs.player_a1.img);
-	mlx_destroy_image(app->mlx, app->imgs.player_a2.img);
-	mlx_destroy_image(app->mlx, app->imgs.wall.img);
-	mlx_destroy_image(app->mlx, app->imgs.collectible.img);
-	mlx_destroy_image(app->mlx, app->imgs.exit.img);
-	if (app->mlx_win)
-		mlx_destroy_window(app->mlx, app->mlx_win);
-	if (app->mlx)
-	{
-		mlx_destroy_display(app->mlx);
-		free(app->mlx);
-	}
-	for (int i = 0; i < app->map.height; i++)
-		free(app->map.cells[i]);
-	free(app->map.cells);
-	free(app->player);
-	free(app->exit);
+	mlx_put_image_to_window(app->mlx, app->mlx_win, app->exit->a1->img, app->exit->x * 32, app->exit->y * 32);
 	for (int i = 0; i < 100; i++)
+	{
 		if (app->collectibles[i])
-			free(app->collectibles[i]);
-	free(app->collectibles);
-	exit(0);
-	return (0);
+			mlx_put_image_to_window(app->mlx, app->mlx_win, app->collectibles[i]->a1->img, app->collectibles[i]->x * 32, app->collectibles[i]->y * 32);
+	}
+	if (time_anim % 10 < 5)
+		mlx_put_image_to_window(app->mlx, app->mlx_win, app->player->a2->img, app->player->x * 32, app->player->y * 32);
+	else
+		mlx_put_image_to_window(app->mlx, app->mlx_win, app->player->a1->img, app->player->x * 32, app->player->y * 32);
+	time_anim++;
 }
 
 void	move_entity(t_app *app, t_entity *entity, int x, int y)
@@ -236,13 +278,15 @@ void	move_entity(t_app *app, t_entity *entity, int x, int y)
 		return ;
 	entity->x += x;
 	entity->y += y;
+	app->nb_moves++;
+	ft_printf("Moves: %d\n", app->nb_moves);
 }
 
 void	player_events(t_app *app)
 {
-	for (int i = 0; app->collectibles[i]; i++)
+	for (int i = 0; i < 100; i++)
 	{
-		if (app->player->x == app->collectibles[i]->x && app->player->y == app->collectibles[i]->y)
+		if (app->collectibles[i] && app->player->x == app->collectibles[i]->x && app->player->y == app->collectibles[i]->y)
 		{
 			free(app->collectibles[i]);
 			app->collectibles[i] = NULL;
@@ -271,15 +315,210 @@ int	handle_key_press(int keycode, t_app *app)
 	return (0);
 }
 
-int	main()
+int check_valid_chars(char **map)
+{
+	int i, j;
+	
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'C' &&
+				map[i][j] != 'E' && map[i][j] != 'P')
+			{
+				ft_printf("Error\nInvalid character in the map.\n");
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int check_required_elements(char **map)
+{
+	int i, j;
+	int count_p = 0, count_e = 0, count_c = 0;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'P') count_p++;
+			if (map[i][j] == 'E') count_e++;
+			if (map[i][j] == 'C') count_c++;
+			j++;
+		}
+		i++;
+	}
+	if (count_p != 1 || count_e != 1 || count_c < 1)
+	{
+		ft_printf("Error\nThe map must contain 1 P, 1 E, and at least 1 C.\n");
+		return (0);
+	}
+	return (1);
+}
+
+int check_rectangle(char **map)
+{
+	int i = 0;
+	int width = strlen(map[0]);
+
+	while (map[i])
+	{
+		if ((int)strlen(map[i]) != width)
+		{
+			ft_printf("Error\nThe map must be rectangular.\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int check_walls(char **map)
+{
+	int i, j;
+	int width = strlen(map[0]);
+	int height = 0;
+
+	while (map[height])
+		height++;
+
+	for (j = 0; j < width; j++)
+	{
+		if (map[0][j] != '1' || map[height - 1][j] != '1')
+		{
+			ft_printf("Error\nThe map must be surrounded by walls.\n");
+			return (0);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		if (map[i][0] != '1' || map[i][width - 1] != '1')
+		{
+			ft_printf("Error\nThe map must be surrounded by walls.\n");
+			return (0);
+		}
+	}
+	return (1);
+}
+
+void flood_fill(char **map, int x, int y)
+{
+	if (map[y][x] == '1' || map[y][x] == 'F') 
+		return;
+
+	map[y][x] = 'F';
+
+	flood_fill(map, x + 1, y);
+	flood_fill(map, x - 1, y);
+	flood_fill(map, x, y + 1);
+	flood_fill(map, x, y - 1);
+}
+
+int	ft_strslen(char **strs)
+{
+	int i;
+
+	for (i = 0; strs[i]; i++);
+	return (i);
+}
+
+char **copy_map_function(char **map)
+{
+	int i;
+	char **copy_map;
+
+	copy_map = malloc(sizeof(char *) * (ft_strslen(map) + 1));
+	for (i = 0; map[i]; i++)
+		copy_map[i] = ft_strdup(map[i]);
+	copy_map[i] = NULL;
+
+	return (copy_map);
+}
+
+void	free_map(char **map)
+{
+	int i;
+
+	for (i = 0; map[i]; i++)
+		free(map[i]);
+	free(map);
+}
+
+int check_valid_path(char **map)
+{
+	int x, y, i, j;
+	char **copy_map;
+
+	for (y = 0; map[y]; y++)
+		for (x = 0; map[y][x]; x++)
+			if (map[y][x] == 'P')
+				goto found;
+found:
+
+	copy_map = copy_map_function(map);
+	flood_fill(copy_map, x, y);
+
+	for (i = 0; copy_map[i]; i++)
+		for (j = 0; copy_map[i][j]; j++)
+			if (copy_map[i][j] == 'C' || copy_map[i][j] == 'E')
+			{
+				ft_printf("Error\nNo valid path to the exit or collectibles.\n");
+				return (free_map(copy_map), 0);
+			}
+
+	return (free_map(copy_map), 1);
+}
+
+void	check_map(t_app *app, char *filename)
+{
+	char	*map_str = get_file_str(filename);
+	if (!map_str)
+	{
+		ft_printf("Error\nUnable to open the file.\n");
+		exit_program(app);
+	}
+	char	**map = ft_split(map_str, '\n');
+
+	if (!check_valid_chars(map)
+		|| !check_required_elements(map)
+		|| !check_rectangle(map)
+		|| !check_walls(map)
+		|| !check_valid_path(map))
+	{
+		free(map_str);
+		free_map(map);
+		exit_program(app);
+	}
+	free(map_str);
+	free_map(map);
+}
+
+int	main(int argc, char **argv)
 {
 	t_app	app;
 
+	if (argc != 2)
+	{
+		ft_printf("Error\nUsage: ./so_long path_to_file.ber\n");
+		return (1);
+	}
 	init_app(&app);
+	init_map(&app, argv[1]);
+	check_map(&app, argv[1]);
+	refresh_screen(&app);
 	draw_map(&app);
 	draw_entities(&app);
 	mlx_hook(app.mlx_win, 17, 0, exit_program, &app);
-	mlx_key_hook(app.mlx_win, handle_key_press, &app);
+	mlx_hook(app.mlx_win, 2, 1L << 0, handle_key_press, &app);
 	mlx_loop(app.mlx);
 	return (0);
 }
